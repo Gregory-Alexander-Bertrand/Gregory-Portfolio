5e6beea3c725325e7250d4dbb0510b6f316c4d2a
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-06-02 11:54:17 -0500

    part of portfolio page done

--- portfolio/src/Pages/Portfolio.js
+++ portfolio/src/Pages/Portfolio.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faReact } from "@fortawesome/free-brands-svg-icons"
+import { faReact, faRProject } from "@fortawesome/free-brands-svg-icons"
+import { faJsSquare} from "@fortawesome/free-brands-svg-icons"
+import { faCss3 } from '@fortawesome/free-brands-svg-icons'
+import { faPython } from '@fortawesome/free-brands-svg-icons'
+import Card from '@material-ui/core/Card'
 
 
 const Portfolio = () => {
     return (
         <div className="portfolio-container">
             <Navbar />
-            <div className="split left">
+            <div className="icon-container">
+            <h1 className="font-icon">
             <FontAwesomeIcon icon={faReact} size='80x'/>
+            </h1>
+            <h1 className="font-icon">
+                <FontAwesomeIcon icon={faJsSquare} />
+            </h1>
+            <h1 className="font-icon">
+                <FontAwesomeIcon icon={faCss3} />
+            </h1>
+            <h1 className="font-icon">
+                <FontAwesomeIcon icon={faPython} />
+            </h1>
             </div>
-            <div className="split right">
+            <div className="project-container">
                 <h1>TESTING</h1>
+                <Card className="project-card">
+                    Testing
+                </Card>
             </div>
         </div>
     )
